e841b39b9a0de51500515de4e9e79e548fdaceaa
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-09-17 18:03:22 +0300

    plans to execute testing suite for temperatureconverter

---
+++ tests/test_randomexercises/test_temperature_converter.py
@@ -0,0 +1,31 @@
+import pytest
+
+from source.random_exercises.mode import create_histogram
+from source.random_exercises.temperature_converter import fahrenheit_to_celsius
+
+
+@pytest.mark.parametrize(
+    "temperature, expected", [
+        (212,)
+    ]
+)
+
+def test_temperature_converter():
+    #Arrange
+    fahrenheit = 212
+    expected = 100
+    #Act
+    fahrenheit_to_celsius(fahrenheit)
+    #Assert
+    assert fahrenheit_to_celsius(fahrenheit) == expected
+    
+    
+def test_
+    
+    
+    
+    
+    
+# assert result == approx(expected)    
+
+# Create parameterized test with expected value using approximation
