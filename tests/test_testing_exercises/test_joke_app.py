54bef154c35e42675902896b238762e5b6415b8a
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-04 15:51:28 +0300

    testing sleep function is between two prints not working. Sleepmock is called before prints.

--- tests/test_testing_exercises/test_joke_app.py
+++ tests/test_testing_exercises/test_joke_app.py
@@ -1,6 +1,9 @@
 import io
 import sys
+from unittest import expectedFailure
 from unittest.mock import patch
+from xml.sax.handler import property_interning_dict
+
 import requests
 from source.testing_exercises.joke_app import get_random_joke, API_URL, print_random_joke
 
@@ -56,19 +59,30 @@ def test_should_print_the_joke_in_correct_order(monkeypatch):
     assert printed_lines == expected_lines
 
 
-def test_should_call_the_sleep_function_between_printing_setup_and_punchline():
+def test_should_call_the_sleep_function_between_printing_setup_and_punchline(monkeypatch):
+
+    mocked_stdout = io.StringIO()
+    monkeypatch.setattr(sys, 'stdout', mocked_stdout)
 
     with patch("source.testing_exercises.joke_app.sleep") as mocked_sleep:
-        mocked_sleep.return_value = None
+        mocked_sleep.return_value = print("called sleep function")
         with patch.object(requests, 'get') as mocked_get:
             mocked_get.return_value.json.return_value = {
                 "id": 1,
                 "setup": "Miksi joulupukki meni psykiatrille?",
                 "punchline": "Hän ei uskonut enää itseensä."
             }
-            print_random_joke()
-
+            print_random_joke()        
+    
+    printed_output = mocked_stdout.getvalue()
+    printed_lines = printed_output.strip().split("\n")
+    expected_lines = ["Miksi joulupukki meni psykiatrille?", "called sleep function", "Hän ei uskonut enää itseensä."]
+    
+    assert printed_lines == expected_lines
     mocked_sleep.assert_called_once_with(2)
+    
+    print(printed_lines)
+    print(expected_lines)
     
        
     
