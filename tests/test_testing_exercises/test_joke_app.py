ee18d3c29887507d76ecc79e7b9cb7c80a4d2948
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-04 15:26:57 +0300

    Fixed API connection.
    Started new testexercise for testShouldCallTheSleepFunctionBetweenPrints

--- tests/test_testing_exercises/test_joke_app.py
+++ tests/test_testing_exercises/test_joke_app.py
@@ -1,17 +1,8 @@
 import io
 import sys
-import time
-from time import sleep
-from xml.sax.handler import property_interning_dict
-
-import requests
-import requests_mock
-
-import source
-from source.testing_exercises.joke_app import get_random_joke, API_URL, print_random_joke
 from unittest.mock import patch
-
-
+import requests
+from source.testing_exercises.joke_app import get_random_joke, API_URL, print_random_joke
 
 mocked_joke = {
     "id": 123,
@@ -42,24 +33,41 @@ def test_should_call_the_api_once():
         get_random_joke()
         mocked_get.assert_called_once_with(API_URL)
         
-def test_should_print_the_joke_in_correct_order(requests_mock, monkeypatch):
+        
+def test_should_print_the_joke_in_correct_order(monkeypatch):
 
     mocked_stdout = io.StringIO()
     monkeypatch.setattr(sys, 'stdout', mocked_stdout)
     
     with patch("source.testing_exercises.joke_app.sleep", return_value=None) as mocked_sleep:
-        requests_mock.get(API_URL, json=mocked_joke)
-        print_random_joke()
+        with patch.object(requests, 'get') as mocked_get:
+            mocked_get.return_value.json.return_value = {
+                "id": 1,
+                "setup": "Miksi joulupukki meni psykiatrille?",
+                "punchline": "Hän ei uskonut enää itseensä."
+            }
+            print_random_joke()
     
     printed_output = mocked_stdout.getvalue()
     printed_lines = printed_output.strip().split("\n")
     expected_lines = ["Miksi joulupukki meni psykiatrille?", "Hän ei uskonut enää itseensä."]
     
-    
     mocked_sleep.assert_called_once_with(2)
-    # assert printed_lines == expected_lines
-        
-    
+    assert printed_lines == expected_lines
+
+
+def test_should_call_the_sleep_function_between_printing_setup_and_punchline():
+
+    with patch("joke_app.sleep", return_value=None) as mocked_sleep:
+        with patch.object(requests, 'get') as mocked_get:
+            mocked_get.return_value.json.return_value = {
+                "id": 1,
+                "setup": "Miksi joulupukki meni psykiatrille?",
+                "punchline": "Hän ei uskonut enää itseensä."
+            }
+            print_random_joke()
+
+    mocked_sleep.assert_called_once_with(2)
     
        
     
