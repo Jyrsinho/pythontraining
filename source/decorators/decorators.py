ab08ac27ced4f3eff14076f4043e981335448969
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-07 12:15:44 +0300

    Timer decorator example

--- source/decorators/decorators.py
+++ source/decorators/decorators.py
@@ -1,7 +1,36 @@
 
+import functools
+import time
+
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f"Finished {func.__name__}() in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+
+def decorator_example(func):
+    @functools.wraps(func)
+    def wrapper_decorator_example(*args, **kwargs):
+        # Do something before
+        value = func(*args, **kwargs)
+        # Do something after
+        return value
+    return wrapper_decorator_example
+
 
 def do_twice(func):
+    @functools.wraps(func)
     def wrapper_do_twice(*args, **kwargs):      # The wrapper_do_twice() inner function now accepts any number of arguments 
         func(*args, **kwargs)                   # and passes them on to the function that it decorates.
-        func(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper_do_twice
+
+
--- source/decorators/using_decorators.py
+++ source/decorators/using_decorators.py
@@ -1,6 +1,4 @@
-from source.decorators.decorators import do_twice
-
-
+from source.decorators.decorators import do_twice, timer
 
 
 @do_twice
@@ -12,6 +10,38 @@ def say_hello(*args, **kwargs):
     else: print("hello")
 
 
+@do_twice
+def return_greeting(name):
+    print("Creating greeting")
+    return f"Hi {name}"
+
+
+@timer
+def waste_some_time(num_times):
+    for i in range(num_times):
+        sum([number**2 for number in range(10_000)])
+
+
+
+
 say_hello()
 say_hello("Timo")
 say_hello("Bob", "Timo")
+
+print()
+print("--------********-------------")
+print()
+
+hi_adam = return_greeting("Adam")
+
+print(hi_adam)
+
+print(say_hello.__name__)
+
+print()
+print("--------********-------------")
+print()
+
+waste_some_time(100)
+waste_some_time(1)
+waste_some_time(10000)
