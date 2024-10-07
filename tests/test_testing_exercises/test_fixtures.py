9fea532a32dc0fd6572893b778a00d48644d4b32
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-07 13:22:43 +0300

    Playing around with pytest fixtures

---
+++ tests/test_testing_exercises/test_fixtures.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.fixture
+def test_data() -> list[str]:
+    return ['a', 'b']
+
+
+@pytest.fixture
+def extended_test_data(test_data) -> list[str]:
+    test_data = test_data +["c"]
+    return test_data
+
+
+def test_data_should_be_list_of_a_and_b(test_data):
+    assert test_data == ['a', 'b']
+    
+
+def test_extended_data_should_be_list_of_a_b_and_c(extended_test_data):
+    assert extended_test_data == ['a', 'b', 'c']
