3d4e399eb63f7ef8452d01cbf11bf5042928f394
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-07 11:50:29 +0300

    using args to call same function with different amount of arguments

--- source/decorators/using_decorators.py
+++ source/decorators/using_decorators.py
@@ -7,8 +7,11 @@ from source.decorators.decorators import do_twice
 def say_hello(*args, **kwargs):
     if len(args) == 1 and isinstance(args[0], str):
         print(f"Hello: {args[0]}")
+    if len(args) == 2:
+        print(f"Hello: {args[0]} and {args[1]}")
     else: print("hello")
 
 
 say_hello()
 say_hello("Timo")
+say_hello("Bob", "Timo")
