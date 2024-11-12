c74af065d4641db9d98b694b34be74b3a2113bc0
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-11-12 17:37:19 +0100

    Stack assigment ready for submit

--- source/stack/stack.py
+++ source/stack/stack.py
@@ -1,11 +1,9 @@
 """
-Kirjoita ohjelma, joka toteuttaa pino-operaatiot push, pop, isEmpty, size ja top pinolle, jonka 
-alkiot ovat kokonaislukuja.
+Class for basic stack.
 
-Toteuta pino-operaatiot itse. (Älä käytä ohjelmakoodien valmiita rutiineja kuten "length"/"count", "append"/
-"add"/"insert" tai "remove", "pop", "delete".)
+Author: Jyri Huhtala
+Date: 2024-12-11
 """
-from inspect import stack
 
 
 class Stack:
@@ -19,9 +17,17 @@ class Stack:
     
     
     def push(self, item):
-        self.size += 1
-        self.elements[0] = item
+        """
+        adds given element to the top of the stack and increases the size of the stack by one. If the stack is full, 
+        capacity of the stack will be doubled.
+        :param item: element to be added
+        """
+        if self.size == self.capacity:
+            self.expand_stack()
         
+        self.elements[self.size] = item
+        self.size += 1
+    
         
     
     def top(self):
@@ -29,12 +35,123 @@ class Stack:
         returns the top element of the stack
         :return: top element of the stack
         """
-        return self.elements[0]
+        return self.elements[self.size-1]
+
+
+
+    def pop(self):
+        """
+        removes the top element from the stack and returns it
+        :return: top element of the stack
+        """
+        if self.size == 0:
+            print("Cannot pop an empty stack")
+            return
+        
+        element_removed = self.elements[self.size-1]
+        self.elements[self.size - 1] = 0
+        self.size -= 1
+        
+        
+        return element_removed
+    
+    
+    def size(self):
+        """
+        returns the size of the stack
+        :return: size of the stack
+        """
+        return self.size
+
+
+    def is_empty(self):
+        """
+        checks if the stack is empty
+        :return: true if the stack is empty, false otherwise
+        """
+        return self.size == 0
+    
+    
+    def expand_stack(self):
+        """
+        Create new twice larger stack and copy all the old stack's elements
+        to it
+        """
+        # create new stack with twice the capacity of the old stack
+        self.capacity = self.capacity * 2
+        new_stack = Stack(self.capacity)
+        
+        # copy all the elements from the old stack to the new stack
+        for i in range(len(self.elements)):
+            new_stack.push(self.elements[i])
+        
+        # replace old elements stack with new larger stack
+        self.elements = new_stack.elements
+
 
 
 def main():
     
-    print ("Testing push- function")
+    print ("Testing push- function:")
+    print ("Created new stack with the capacity of 3")
+    stack = Stack(3)
+    print ("The size of the stack is " + str(stack.size))
+    print("Executed stack.push(1)")
+    stack.push(1)
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("The capacity of the stack is " + str(stack.capacity))
+    print("Executed stack.push(2)")
+    stack.push(2)
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("The capacity of the stack is " + str(stack.capacity))
+    print("Executed stack.push(3)")
+    stack.push(3)
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("The capacity of the stack is " + str(stack.capacity))
+    print("Executed stack.push(4)")
+    stack.push(4)
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("The capacity of the stack is " + str(stack.capacity))
+    
+    
+    print()
+    print ("Testing pop- function:")
+    print ("The size of the stack is " + str(stack.size))
+    print ("The top element of the stack is " + str(stack.top()))
+    print("Executed stack.pop()")
+    stack.pop()
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("Executed stack.pop()")
+    stack.pop()
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("Executed stack.pop()")
+    stack.pop()
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("Executed stack.pop()")
+    stack.pop()
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("Executed stack.pop()")
+    stack.pop()
+    print("The size of the stack is " + str(stack.size))
+    print("The top element of the stack is " + str(stack.top()))
+    print("Executed stack.pop()")
+    
+    print ()
+    print("Testing is empty-function")
+    print("Executing stack.is_empty()")
+    print ("Result of the function is:" + str(stack.is_empty()))
+    print("Executed stack.push(1)")
+    stack.push(1)
+    print("Executing stack.is_empty()")
+    print ("Result of the function is:" + str(stack.is_empty()))    
     
 
 if __name__ == "__main__":
--- tests/test_stack/test_stack.py
+++ tests/test_stack/test_stack.py
@@ -1,4 +1,5 @@
 import pytest
+from robot.utils.asserts import assert_true, assert_false
 
 from source.stack.stack import Stack
 
@@ -12,17 +13,57 @@ Toteuta pino-operaatiot itse. (Älä käytä ohjelmakoodien valmiita rutiineja k
 
 @pytest.fixture
 def setup_stack():
-    testStack = Stack(5)
-    return testStack
+    test_stack = Stack(5)
+    return test_stack
 
-def test_should_add_elements_to_stack(setup_stack):
-    testStack = setup_stack
-    testStack.push(1)
-    assert testStack.size == 1
+def test_push_should_increase_the_size_of_the_stack(setup_stack):
+    test_stack = setup_stack
+    test_stack.push(1)
+    assert test_stack.size == 1
     
-def test_should_add_elements_to_stack2():
     
-    testStack = Stack(5)
-    testStack.push(1)
-    testStack.push(2)
-    assert testStack.top() == 
+def test_should_add_an_element_to_stack():
+    
+    test_stack = Stack(5)
+    test_stack.push(1)
+    assert test_stack.top() == 1
+    
+    
+def test_should_add_two_elements_to_stack():
+    test_stack = Stack(5)
+    test_stack.push(1)
+    test_stack.push(2)
+    
+    assert test_stack.pop() == 2
+    assert test_stack.pop() == 1
+    
+    
+def test_should_create_larger_stack_when_array_size_is_exceeded():
+    test_stack = Stack(3)
+    test_stack.push(1)
+    test_stack.push(2)
+    test_stack.push(3)
+    test_stack.push(4)
+    
+    assert test_stack.size == 4
+    
+    
+def test_should_be_able_to_add_values_to_a_stack_that_has_been_expanded():
+    test_stack = Stack(1)
+    test_stack.push(1)
+    test_stack.push(2)
+    test_stack.push(3)
+    
+    assert test_stack.top() == 3
+    
+    
+def test_should_return_true_for_is_empty_when_stack_is_empty():
+    test_stack = Stack(5)
+    assert_true(test_stack.is_empty())
+    
+    
+def test_should_return_false_when_stack_has_an_element():
+    test_stack = Stack(5)
+    test_stack.push(1)
+    assert_false(test_stack.is_empty())
+    
